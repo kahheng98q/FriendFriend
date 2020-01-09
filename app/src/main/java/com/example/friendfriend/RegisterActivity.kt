58c5062d34c259fc37f4783c7a47d6b5ee58393f
kahheng <kahheng98q@gmail.com>
2020-01-10 04:50:59 +0800

    done half

--- app/src/main/java/com/example/friendfriend/RegisterActivity.kt
+++ app/src/main/java/com/example/friendfriend/RegisterActivity.kt
@@ -12,12 +12,15 @@ import android.util.Log
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.parcel.Parcelize
 import kotlinx.android.synthetic.main.register_activity.*
 import java.util.*
 
 class RegisterActivity : AppCompatActivity() {
+    var note: HashMap<String, Object> = HashMap<String, Object>()
+    lateinit var nEmail: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,6 +44,7 @@ class RegisterActivity : AppCompatActivity() {
     }
     var ImageUri: Uri? = null
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == 0 && resultCode == Activity.RESULT_OK && data!= null){
             ImageUri = data.data
@@ -55,13 +59,17 @@ class RegisterActivity : AppCompatActivity() {
         val email = edt_email.text.toString()
         val password = edt_password.text.toString()
 
+        nEmail=edt_email.text.toString().toLowerCase()
         if(email.isEmpty() || password.isEmpty()){
             Toast.makeText(baseContext, "Please enter text in Email or Password.", Toast.LENGTH_SHORT).show()
         }
         Log.d("RegisterActivity","Email is: " + email )
         Log.d("RegisterActivity", "Password is: "+ password)
+
         FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
+                note.put("Email", nEmail as Object)
+
                 uploadImage()
             }
             .addOnFailureListener{
@@ -79,7 +87,9 @@ class RegisterActivity : AppCompatActivity() {
                 Log.d("RegisterActivity", "Successfully uploaded: ${it.metadata?.path}")
                 ref.downloadUrl.addOnSuccessListener {
                     Log.d("RegisterActivity","File Location: $it")
+                    note.put("Image", ImageUri.toString() as Object)
                     saveUser(it.toString())
+
                 }
             }
             .addOnFailureListener{
@@ -89,21 +99,43 @@ class RegisterActivity : AppCompatActivity() {
     private fun saveUser(profileImageUrl: String){
         val uid = FirebaseAuth.getInstance().uid ?: ""
         val ref = FirebaseDatabase.getInstance().getReference("/users/$uid")
+
         val user = User(uid, edt_username.text.toString(), profileImageUrl)
         val un =edt_username.text.toString()
+        note.put("Name", edt_username.text.toString() as Object)
+
         Log.d("RegisterActivity","$uid")
         Log.d("RegisterActivity","$un")
         Log.d("RegisterActivity","$profileImageUrl")
+
+
         ref.setValue(user)
             .addOnSuccessListener{
                 Log.d("RegisterActivity","Done saved")
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
+
+                storeToFirestore()
+
 
             }.addOnFailureListener{
                 Log.d("RegisterActivity", "Failed to upload user")
             }
+
+
     }
+    fun storeToFirestore(){
+        val db = FirebaseFirestore.getInstance()
+        db.collection("New").document(nEmail)
+            .set(note)
+            .addOnSuccessListener {
+                Toast.makeText(applicationContext, "Done", Toast.LENGTH_SHORT)
+                    .show()
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }.addOnFailureListener{
+                Log.d("RegisterActivity", "Failed to upload user")
+            }
+    }
+
 }
 @Parcelize
 class User(val uid:String,val username: String, val profileImageUrl: String): Parcelable{
